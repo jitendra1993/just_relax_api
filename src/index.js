5b497f6654f599a9d9e3b128f092387ff263b8f5
jitendra <jitendra.agrawal@w3villa.com>
2022-02-26 15:20:47 +0530

    ddddd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import {Server} from './server.js';
 process.env.TZ = 'Europe/London'
 let server = new Server().app;
-let port = process.env.PORT || 4100;
+let port = process.env.PORT || 5300;
 server.listen(port, () => {
     console.log('server is running on', port);
 });
