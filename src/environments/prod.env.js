50e7a2d11917f0bb3b5cf6d3f1b9401cdd1fcd9a
jitendra <jitendra.agrawal@w3villa.com>
2022-02-26 15:31:53 +0530

    aaa

--- src/environments/prod.env.js
+++ src/environments/prod.env.js
@@ -1,6 +1,6 @@
 //import {Environment} from './env.js';
 
 export const ProdEnvironment = {
-    db_url:'mongodb+srv://jitendra:'+encodeURIComponent('welcome@123#')+'@mongodb.ib1dt.mongodb.net/hungry?retryWrites=true&w=majority',
+    db_url:'mongodb+srv://jitendra:'+encodeURIComponent('welcome@123#')+'@mongodb.ib1dt.mongodb.net/just_relax?retryWrites=true&w=majority',
     jwt_secret: 'prodSecret'
 };
